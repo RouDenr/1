51a42a9e7cf323b531672fa80d3428a4578198a2
roudenr <mrroudenr@gmail.com>
2020-10-08 21:08:20 +0700

    make it better

--- main.go
+++ main.go
@@ -6,7 +6,12 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	_ "github.com/heroku/x/hmetrics/onload"
+)
+
+const (
+	webhook = "https://afternoon-wave-60151.herokuapp.com/"
 )
 
 func main() {
@@ -26,4 +31,22 @@ func main() {
 	})
 
 	router.Run(":" + port)
+
+	bot, err := tgbotapi.NewBotAPI("1114854329:AAGIXlmgZzvmdz16Av-w-wcd2-EVOjZnMSU")
+	if err != nil {
+		log.Fatal("cr bot: ", err)
+	}
+	log.Println("bot cr")
+
+	if _, err := bot.SetWebhook(tgbotapi.NewWebhook(webhook)); err != nil {
+		log.Fatalf("setting webhook %v; error: %v", webhook, err)
+	}
+	log.Println("webhook set")
+
+	updates := bot.ListenForWebhook("/")
+	for updates := range updates {
+		if _, err := bot.Send(tgbotapi.NewMessage(updates.Message.Chat.ID, "эй")); err != nil {
+			log.Print(err)
+		}
+	}
 }
